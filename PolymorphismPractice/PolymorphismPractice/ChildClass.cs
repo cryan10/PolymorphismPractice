d64126561ace46718b8ce36a3fce7a9d24187760
Carrie Ryan <cryan1205@gmail.com>
2017-03-07 21:54:30 -0500

    Initial commit

---
+++ PolymorphismPractice/PolymorphismPractice/ChildClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismPractice
+{
+    class ChildClass : ParentClass
+    {
+        //override can only used in child classes; it allows for specific instructions 
+        public override void Clean()
+        {
+            base.Clean(); //calls method from parent class
+            //then add code to override parent class
+        }
+
+        public override void Cook()
+        {
+            base.Cook();
+        }
+
+    }
+}
---
+++ PolymorphismPractice/PolymorphismPractice/ParentClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolymorphismPractice
+{
+    class ParentClass
+    {
+        // virtual only exists in parent class
+        public virtual void Clean()
+        {
+            Console.WriteLine("Let's clean the kitchen");
+        }
+
+        public virtual void Cook()
+        {
+            Console.WriteLine("Let's cook some food");
+        }
+
+   
+    }
+}
